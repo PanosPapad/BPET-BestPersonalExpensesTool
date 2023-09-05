1cf8aaeca9e7049f956a53b69969d7379f23161d
Panos Papadopoulos <panos.papadopoulos@teampicnic.com>
2023-09-06 00:09:42 +0200

    Add content to the constants

---
+++ src/constans_mock.py
@@ -0,0 +1,13 @@
+expense_categories = {
+    "Housing Expenses": ["Rent"],
+    "Food and Dining": ["Groceries", "Coffee"],
+    "Transportation": ["Train", "Gas"],
+}
+
+filter_words = {
+    "NS": ["Train"],
+    "Starbucks": ["Coffee"],
+    "Lidl": ["Groceries"],
+    "Scamgency": ["Rent"]
+}
+
